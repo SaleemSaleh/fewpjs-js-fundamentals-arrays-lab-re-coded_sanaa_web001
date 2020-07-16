aa4ea49b3cd079449f7f7ab963c481981dda5545
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-16 16:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution here!
-const append = ["Milo", "Otis", "Garfield"];
-const prepend = ["Milo", "Otis", "Garfield"];
-const removeLast  = ["Milo", "Otis", "Garfield"];
-const removeFirst  = ["Milo", "Otis", "Garfield"];
+let append = ["Milo", "Otis", "Garfield"];
+let prepend = ["Milo", "Otis", "Garfield"];
+let removeLast  = ["Milo", "Otis", "Garfield"];
+let removeFirst  = ["Milo", "Otis", "Garfield"];
 
 prepend.push("Odie");
